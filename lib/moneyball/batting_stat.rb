b2908e741abd7a0a091abf44593c86ea13505994
Jeremy Ward <jrmy.ward@gmail.com>
2014-03-29 11:29:57 -0700

    Added a command to determine the League's Triple Crown winner for a given year.

--- lib/moneyball/batting_stat.rb
+++ lib/moneyball/batting_stat.rb
@@ -3,13 +3,36 @@ module Moneyball
     belongs_to :player, class_name: "Moneyball::Player", foreign_key: "player_id", primary_key: "player_id"
     delegate :first_name, :last_name, :full_name, :birth_year, to: :player, allow_nil: true, prefix: "player"
 
-    def self.season_roster(team_id, year)
-      where(team_id: team_id).where(year: year)
+    scope :for_team, ->(team) { where(team_id: team) }
+    scope :for_year, ->(year) { where(year: year) }
+    scope :home_run_leader, -> { order("home_runs desc").limit(1) }
+    scope :in_league, ->(league) { where(league: league) }
+    scope :rbi_leader, -> { order("runs_batted_in desc").limit(1) }
+
+    def self.batting_avg_leader_amongst(players)
+      winner = players[0] # assume the first player is the winner to start
+      players.each do |player|
+        winner = player if player.batting_avg > winner.batting_avg
+      end
+      return winner
     end
 
     def self.team_slugging_percentage(team_id, year)
       players = season_roster(team_id, year)
       team_slugging_percentage = (players.map(&:slugging_percentage).reduce(:+).to_f / players.count).round(3)
+    end
+
+    def self.triple_crown_winner(league, year)
+      hr_leader  = self.for_year(year).in_league(league).home_run_leader.first
+      rbi_leader = self.for_year(year).in_league(league).rbi_leader.first
+      return "(No winner)" unless hr_leader.player_id == rbi_leader.player_id
+
+      players = self.for_year(year).in_league(league).where("at_bats >= ?", 400)
+      if self.batting_avg_leader_amongst(players).player_id == hr_leader.player_id
+        hr_leader.player_full_name
+      else
+        "(No winner)"
+      end
     end
 
     # Batting average = hits / at-bats
